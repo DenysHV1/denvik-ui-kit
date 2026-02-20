aa1665c29a2f3cc9c8270d666151c503c58d67f3
Denys Harkusha <denystech@godsofmarketing.network>
2026-02-20 22:06:45 +0100

    new files

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export * from "./libs/cn";
 //ui
 export * from "./components/ui/button";
 export * from "./components/ui/spinner";
+export * from "./components/ui/checkbox";
+export * from "./components/ui/input";
+export * from "./components/ui/textarea";
+export * from "./components/theme";
